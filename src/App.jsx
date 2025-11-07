3c27eb7cfbbc6a2a16fafddb76fc483f02ae7c4a
Vicky Pineli <v.pinero70@gmail.com>
2025-11-07 12:34:18 +0100

    instalacion y configuracion de sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 // src/App.jsx
-import './App.css'
+import { useState } from "react";
 import Auth from "./src/components/Auth";
+import Profile from "./src/components/Profile";
+import BookingForm from "./src/components/BookingForm";
+import './styles/main.scss';
 
 
 function App() {
+  const [view, setView] = useState("auth");
+
   return (
-    <div>
-      <Auth />
+    <div style={{ padding: 20 }}>
+      <div style={{ marginBottom: 20 }}>
+        <button onClick={() => setView("auth")}>Login</button>
+        <button onClick={() => setView("profile")}>Perfil</button>
+        <button onClick={() => setView("booking")}>Reservar Txoko</button>
+      </div>
+      <hr />
+      {view === "auth" && <Auth />}
+      {view === "profile" && <Profile />}
+      {view === "booking" && <BookingForm />}
     </div>
   );
 }
 
-export default App
+export default App;
+
+
+
+
