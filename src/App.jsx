4e9b19e588d6521822f9383673bf10cb7cb03509
Vicky Pineli <v.pinero70@gmail.com>
2025-11-08 18:26:37 +0100

    instalacion react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,22 @@
 // src/App.jsx
-import { useState } from "react";
-import Auth from "./src/components/Auth";
-import Profile from "./src/components/Profile";
-import BookingForm from "./src/components/BookingForm";
-import './styles/main.scss';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SplashScreen from "./pages/SplashScreen";
+// import AuthPage from "./pages/AuthPage";
+// import HomePage from "./pages/HomePage";
+// import ProfilePage from "./pages/ProfilePage";
+// import AdminPage from "./pages/AdminPage";
 
 function App() {
-  const [view, setView] = useState("auth");
-
   return (
-    <div style={{ padding: 20 }}>
-      <div style={{ marginBottom: 20 }}>
-        <button onClick={() => setView("auth")}>Login</button>
-        <button onClick={() => setView("profile")}>Perfil</button>
-        <button onClick={() => setView("booking")}>Reservar Txoko</button>
-      </div>
-      <hr />
-      {view === "auth" && <Auth />}
-      {view === "profile" && <Profile />}
-      {view === "booking" && <BookingForm />}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<SplashScreen />} />
+        {/* <Route path="/auth" element={<AuthPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/admin" element={<AdminPage />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
