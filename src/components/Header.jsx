71baad90479484b48e0212b7b94849b90deb18c9
Vicky Pineli <v.pinero70@gmail.com>
2025-11-25 15:46:41 +0100

    changes in styles header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { signOut } from "firebase/auth";
 import { auth } from "../firebaseConfig";
 
-import { Utensils } from "lucide-react";
+import { Utensils, Calendar, User, LogOut, ShieldCheck } from "lucide-react";
 import { useAuth } from "../context/AuthContext";
 import "../styles/components/Header.scss";
 
@@ -28,22 +28,24 @@ function Header() {
         {user && profile && (
           <div className="user-header">
             <p className="user-name" onClick={() => navigate("/profile")}>
-              Hola, {profile.nombre} {profile.apellidos}
+             ¡Hola {profile.nombre} !
             </p>
 
             <div className="user-actions">
                <button 
                   onClick={() => navigate("/home")}
                   className="btn-small back">
-                  Inicio
+                  <Calendar size={18} />
+                  <span>Calendario</span>
                </button>
 
               {isAdmin && (
                 <button
                   onClick={() => navigate("/admin")}
-                  className="btn-small admin"
+                  className="btn-small admin" 
                 >
-                  Admin
+                  <ShieldCheck size={18} />
+                  <span>Panel de Administración</span>
                 </button>
 
               )}
@@ -52,12 +54,14 @@ function Header() {
                   onClick={() => navigate("/profile")}
                   className="btn-small profile"
                 >
-                  Mi Perfil
+                  <User size={18} />
+                  <span>Mi Perfil</span>
                 </button>
               )}
 
               <button onClick={handleLogout} className="btn-small logout">
-                Cerrar Sesión
+                <LogOut size={18} />
+                <span>Salir</span>
               </button>
             </div>
           </div>
